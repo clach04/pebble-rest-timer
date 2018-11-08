d7a5caf8fa52458c275b961de9eee7ddb8993511
clach04 <Chris.Clark@actian.com>
2018-11-07 22:15:17 -0800

    experiment with Clay in emulator
    Plan was to redirect to static non-Clay config for emulator
    this WIP may remove the need for that idea

--- src/pkjs/app.js
+++ src/pkjs/app.js
@@ -1,3 +1,36 @@
 var Clay = require('./clay.js');
 var clayConfig = require('./config');
-var clay = new Clay(clayConfig);
+//var clay = new Clay(clayConfig);
+var clay = new Clay(clayConfig, null, { autoHandleEvents: false });
+
+
+// as autoHandleEvents set to false need a basic showConfiguration action
+Pebble.addEventListener('showConfiguration', function(e) {
+	// for what ever reason this then causes Clay display to work in emulatpr
+	// // NOTE save works with web app, but then get error and pebble never receives data/config with my quick test
+	var emulator = !Pebble || Pebble.platform === 'pypkjs';
+        console.log('showConfiguration');
+    console.log('emulator  =' + JSON.stringify(emulator));
+
+    Pebble.openURL(clay.generateUrl());
+});
+
+Pebble.addEventListener('webviewclosed', function(e) {
+    if (e && !e.response) {
+    return;
+    }
+
+    // Get the keys and values from each config item
+    var dict = clay.getSettings(e.response);
+
+    console.log('string config data =' + JSON.stringify(dict));
+    console.log('string config data length=' + JSON.stringify(dict).length);
+    // Send settings values to watch side
+    Pebble.sendAppMessage(dict, function(e) {
+        console.log('Sent config data to Pebble');
+    }, function(e) {
+        console.log('Failed to send config data!');
+        //console.log(JSON.stringify(e));
+    });
+});
+
